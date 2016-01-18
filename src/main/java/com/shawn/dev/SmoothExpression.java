0d04f63fe4796c1391771f736bb1fef4021a8d42
shawn417 <qijun.shen@outlook.com>
2016-01-18 21:26:48 +0800

    add ignore file SmoothExpression.java and Tests added

---
+++ src/main/java/com/shawn/dev/SmoothExpression.java
@@ -0,0 +1,13 @@
+package com.shawn.dev;
+
+public class SmoothExpression {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void sayHi() {
+        System.out.println("Hello from SmoothExpression");
+    }
+
+}
---
+++ src/test/java/com/shawn/dev/SmoothExpressionTest.java
@@ -0,0 +1,17 @@
+package com.shawn.dev;
+
+import org.junit.Test;
+
+public class SmoothExpressionTest {
+
+    @Test
+    public void testInit() {
+        SmoothExpression se = new SmoothExpression();
+    }
+
+    @Test
+    public void testSayHi() {
+        SmoothExpression se = new SmoothExpression();
+        se.sayHi();
+    }
+}
